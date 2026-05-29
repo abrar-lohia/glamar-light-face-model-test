39f097ddce4399ace40f4ccae36b4d74dd5aeefb
abrarlohia <abrarlohia@gofynd.com>
2026-05-29 12:17:29 +0000

    updated webhook url

--- indexBlob.js
+++ indexBlob.js
@@ -16,7 +16,7 @@ const BAR_COLORS = ['#ef4444', '#facc15', '#a78bfa', '#22c55e'];
 const CONFIDENCE_THRESHOLD = 0.4;
 const STABLE_FRAMES = 4;
 const PERF_INTERVAL_MS = 12000;
-const WEBHOOK_URL = 'https://webhook.site/0607823d-b31f-470f-a4b5-824646924b8e';
+const WEBHOOK_URL = 'https://mxytq63oik.rbmock.dev';
 
 let CANVAS_W = 480;
 let CANVAS_H = 360;
